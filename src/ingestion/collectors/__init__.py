5df9895bb79fc02de8a8f7f64156f6cb86c6f39d
yassinebrahem2 <yassine.brahem081@gmail.com>
2026-02-11 22:25:08 +0100

    feat: add DocumentCollector base class for news/text sources

--- src/ingestion/collectors/__init__.py
+++ src/ingestion/collectors/__init__.py
@@ -2,12 +2,14 @@
 
 from src.ingestion.collectors.base_collector import BaseCollector
 from src.ingestion.collectors.calendar_collector import EconomicCalendarCollector
+from src.ingestion.collectors.document_collector import DocumentCollector
 from src.ingestion.collectors.ecb_collector import ECBCollector
 from src.ingestion.collectors.fred_collector import FREDCollector
 from src.ingestion.collectors.mt5_collector import MT5Collector
 
 __all__ = [
     "BaseCollector",
+    "DocumentCollector",
     "EconomicCalendarCollector",
     "ECBCollector",
     "FREDCollector",
