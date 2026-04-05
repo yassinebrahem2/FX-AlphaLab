30a6944a452e9786b4457226bc639f83176c8b8b
zahia <zahiaziz444@gmail.com>
2026-04-05 19:48:37 +0100

    fix(preprocessors): remove NewsPreprocessor dependency to isolate calendar pipeline

--- src/ingestion/preprocessors/__init__.py
+++ src/ingestion/preprocessors/__init__.py
@@ -4,7 +4,7 @@ from src.ingestion.preprocessors.base_preprocessor import BasePreprocessor
 from src.ingestion.preprocessors.calendar_parser import CalendarPreprocessor
 from src.ingestion.preprocessors.document_preprocessor import DocumentPreprocessor
 from src.ingestion.preprocessors.macro_normalizer import MacroNormalizer
-from src.ingestion.preprocessors.news_preprocessor import NewsPreprocessor
+"from src.ingestion.preprocessors.news_preprocessor import NewsPreprocessor"
 from src.ingestion.preprocessors.price_normalizer import PriceNormalizer
 
 __all__ = [
